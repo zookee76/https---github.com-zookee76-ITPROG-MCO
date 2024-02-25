79c421d35a70a50a73c1222d54148ae567c2fafa
ArgosNevrikos <mykel_tiu@dlsu.edu.ph>
2024-02-25 21:39:33 +0800

    update and added new files

---
+++ itprog-mco/api/index.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+    include("DbConnect.php");
+    $conn = new DbConnect();
+    $db = $conn->connect();
+
+    $method = $_SERVER['REQUEST_METHOD'];
+    switch($method) {
+        case 'GET':
+            $sql = "SELECT * FROM item";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $item = $stmt->fetchAll(PDO::FETCH_ASSOC);        
+            echo json_encode($item);
+            break;
+    }
+
+?>
--- itprog-mco/components/productList.jsx
+++ itprog-mco/components/productList.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import Counter from './counter';
 import './productList.css';
 import { BsFillCakeFill } from "react-icons/bs";
@@ -7,6 +9,16 @@ import { TbCandy } from "react-icons/tb";
 import { LuCroissant } from "react-icons/lu";
 
 function ProductList() {
+  const [item, setitem] = useState([]);
+  useEffect(() => {
+    getitem();
+  }, []);
+  function getitem() {
+      axios.get('http://localhost/api/').then(function(response) {
+          console.log(response.data);
+          setitem(response.data);
+      });
+  }
   const [productCounts, setProductCounts] = useState({});
 
   const onCountChange = (productId, newCount) => {
@@ -23,17 +35,6 @@ function ProductList() {
     setSelectedCategory(categoryId);
   };
 
-  const products = [
-    { id: 1, category: 1, name: 'Product 1', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 2, category: 1, name: 'Product 2', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 3, category: 2, name: 'Product 3', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 4, category: 2, name: 'Product 4', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 5, category: 3, name: 'Product 5', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 6, category: 3, name: 'Product 6', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 7, category: 4, name: 'Product 7', price: 7.5, stock: 10, initialCount: 0 },
-    { id: 8, category: 4, name: 'Product 8', price: 7.5, stock: 10, initialCount: 0 },
-  ]; /* THESE ARE TEMPORARY HARDCODED PRODUCT VALUES SINCE BACKEND NEEDS TO BE CODED */
-
   return (
     <div className="product-filter">
       <button onClick={() => handleCategorySelect(null)} className="category-button">All</button>
@@ -43,16 +44,16 @@ function ProductList() {
       <button onClick={() => handleCategorySelect(4)} className="category-button">Cake</button>
 
       <div className="product-container">
-          {products.filter(product => selectedCategory === null || product.category === selectedCategory)
+          {item.filter(product => selectedCategory === null || product.category === selectedCategory)
             .map(product => (
-              <div key={product.id} className="individual-product-container">
+              <div key={product.item_id} className="individual-product-container">
                 <div className="icon-container">
-                  {product.category == 1 && <GiChocolateBar />}
-                  {product.category == 2 && <TbCandy />}
-                  {product.category == 3 && <LuCroissant />}
-                  {product.category == 4 && <BsFillCakeFill />}
+                  {product.category_id == 1 && <GiChocolateBar />}
+                  {product.category_id == 2 && <TbCandy />}
+                  {product.category_id == 3 && <LuCroissant />}
+                  {product.category_id == 4 && <BsFillCakeFill />}
                 </div>
-                <h3>{product.name}</h3>
+                <h3>{product.item_name}</h3>
                 <Counter 
                     product={product} 
                     onCountChange={onCountChange} 
@@ -63,11 +64,11 @@ function ProductList() {
 
                 <div className="price-stock-container">
                     <div className="price-container">
-                        <h4>₱{product.price}</h4>
+                        <h4>₱{product.item_price}</h4>
                     </div>
                     
                     <div className="stock-container">
-                        <h4>{product.stock} Pieces Left</h4>
+                        <h4>{product.item_stock} Pieces Left</h4>
                     </div>
                 </div>
               </div>
